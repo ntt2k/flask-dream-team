bfe0da866d009e898ca270f49b64b67ef9ce4c2a
Trung Nguyen <ntt2k@pop-os.localdomain>
2018-11-24 14:02:25 -0600

    implement register & login

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,16 @@
 
 # third-party imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
-
 login_manager = LoginManager()
 
 
@@ -19,16 +19,17 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    db.init_app(app)
 
+    Bootstrap(app)
+    db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-
     migrate = Migrate(app, db)
 
     from app import models
 
+    # blueprint registration
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
